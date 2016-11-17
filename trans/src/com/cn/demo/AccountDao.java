b0ebde1399debca24f51d75cef44ea3551baf34f
wilbur <wilburwong1991@gmail.com>
2016-11-17 17:17:43 +0800

    del

--- trans/src/com/cn/demo/AccountDao.java
+++
@@ -1,8 +0,0 @@
-package com.cn.demo;
-
- interface AccountDao {
-
-    public void outMoney(String out, Double money);
-    
-    public void inMoney(String in, Double money);
-}
--- trans/src/com/cn/demo/AccountService.java
+++
@@ -1,6 +0,0 @@
-package com.cn.demo;
-
-public interface AccountService {
-
-    public void transfer(String out, String in, Double money);
-}
